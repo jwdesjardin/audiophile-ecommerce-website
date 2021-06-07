88959e9b01aca5c5ad39bad218d36a3f0de2bf93
joey desjardins <jwdesjardin@gmail.com>
2021-06-07 10:21:44 -0700

    rename pages as .tsx files

---
+++ my-project/pages/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import sanityClient from '../lib/client'
+import { getProductsByCategory } from '../lib/query'
+import React from 'react'
+
+export default function Home() {
+	// React.useEffect(() => {
+	// 	const myFetch = async () => {
+	// 		const data = await sanityClient.fetch(getProductsByCategory)
+	// 		console.log(data)
+	// 		return data
+	// 	}
+	// 	myFetch()
+	// }, [])
+
+	return (
+		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
+			<Head>
+				<title>Create Next App</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main className=''>
+				<p className='subtitle'>Index</p>
+				<p className='overline-text'>Index</p>
+				{/* <button className='button-one'>See Product</button> */}
+				<h1 className='h1'>Morbi interdum mollis sapien</h1>
+				<div className='mb-10'></div>
+				<h1 className='h2'>Donec nec justo eget felis facilisis</h1>
+				<div className='mb-10'></div>
+				<h1 className='h3'>Nunc sem lacus accum</h1>
+				<div className='mb-10'></div>
+				<h1 className='h4'>interdum consectetuer</h1>
+				<div className='mb-10'></div>
+				<h1 className='h5'>nascetur ridiculus mus</h1>
+				<div className='mb-10'></div>
+				<h1 className='h6'>natoque penatibus et</h1>
+			</main>
+		</div>
+	)
+}
