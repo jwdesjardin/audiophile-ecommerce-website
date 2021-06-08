1ce824cbfe55455fd268b53977e46775d9142992
joey desjardins <jwdesjardin@gmail.com>
2021-06-08 16:08:59 -0700

    add hover effect to navbar and imporve link styles

--- my-project/components/Layout/Navbar.tsx
+++ my-project/components/Layout/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const Navbar = ({ header }: { header?: string }) => {
+	const linkHoverEffect = 'hover:text-orange-600 transition'
+
 	return (
 		<div className='bg-black-900 text-white-100  '>
 			<div className='flex items-center justify-between md:justify-start lg:justify-between py-8 content-container'>
@@ -9,18 +11,18 @@ const Navbar = ({ header }: { header?: string }) => {
 				<div className='md:ml-10 lg:ml-0'>
 					<img src='/logo.svg' alt='' />
 				</div>
-				<div className='text-xs uppercase leading-[1.5625rem] tracking-[.0625rem]  space-x-10 hidden lg:flex'>
+				<div className='navlink  space-x-10 hidden lg:flex'>
 					<Link href='/' passHref>
-						<a>Home</a>
+						<a className={linkHoverEffect}>Home</a>
 					</Link>
 					<Link href='/headphones' passHref>
-						<a>Headphones</a>
+						<a className={linkHoverEffect}>Headphones</a>
 					</Link>
 					<Link href='/speakers' passHref>
-						<a>Speakers</a>
+						<a className={linkHoverEffect}>Speakers</a>
 					</Link>
 					<Link href='/earphones' passHref>
-						<a>Earphones</a>
+						<a className={linkHoverEffect}>Earphones</a>
 					</Link>
 				</div>
 				<button className='shopping-cart-button md:ml-auto lg:ml-0 '></button>
