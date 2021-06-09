779e6c244317f894d008d492158c8cd6c07f30cd
joey desjardins <jwdesjardin@gmail.com>
2021-06-09 15:55:30 -0700

    add dynamic data to ProductPreview

--- my-project/components/Category/ProductPreview.tsx
+++ my-project/components/Category/ProductPreview.tsx
@@ -1,39 +1,62 @@
+import Link from 'next/link'
 import React from 'react'
 
-const ProductPreview = () => {
+const ProductPreview = ({ flexReverse = true }: { flexReverse?: boolean }) => {
 	return (
-		<>
-			<div className='content-container mt-16 md:mt-28 lg:mt-40 flex flex-col items-center lg:justify-between lg:flex-row'>
-				<Image className='mb-8 md:mb-12 ' />
-				<TextContent className=''></TextContent>
-			</div>
-			<div className='content-container mt-16 md:mt-28 lg:mt-40 flex flex-col items-center lg:justify-between lg:flex-row-reverse'>
-				<Image className='mb-8 md:mb-12' />
-				<TextContent className=''></TextContent>
-			</div>
-		</>
+		<div
+			className={
+				'content-container mt-16 md:mt-28 lg:mt-40 flex flex-col items-center lg:justify-between' +
+				`${flexReverse ? ' lg:flex-row-reverse' : ' lg:flex-row'}`
+			}
+		>
+			<Image className='mb-8 md:mb-12' mobileImage='' tabletImage='' desktopImage='' />
+			<TextContent
+				className=''
+				isNew={true}
+				title='XX59 Headphones'
+				description='The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium 
+  headphone experience by reproducing the balanced depth and precision of studio-quality sound.'
+				slug='xx59-headphones'
+			></TextContent>
+		</div>
 	)
 }
 
-const Image = ({ className = '' }: { className?: string }) => {
+const Image = ({
+	className = '',
+	mobileImage,
+	tabletImage,
+	desktopImage,
+}: {
+	className?: string
+	mobileImage: string
+	tabletImage: string
+	desktopImage: string
+}) => {
 	return (
 		<div className={'rounded-lg overflow-hidden max-w-[540px]' + ` ${className}`}>
 			<picture>
-				<source
-					media='(min-width: 1110px)'
-					srcSet='/assets/category-headphones/desktop/image-xx59.jpg'
-				/>
-				<source
-					media='(min-width: 768px)'
-					srcSet='/assets/category-headphones/tablet/image-xx59.jpg'
-				/>
-				<img src='/assets/category-headphones/mobile/image-xx59.jpg' alt='xx59 headphones' />
+				<source media='(min-width: 1110px)' srcSet={desktopImage} />
+				<source media='(min-width: 768px)' srcSet={tabletImage} />
+				<img src={mobileImage} alt='xx59 headphones' />
 			</picture>
 		</div>
 	)
 }
 
-const TextContent = ({ className = '' }: { className?: string }) => {
+const TextContent = ({
+	className = '',
+	isNew,
+	title,
+	description,
+	slug,
+}: {
+	className?: string
+	isNew: boolean
+	title: string
+	description: string
+	slug: string
+}) => {
 	return (
 		<div
 			className={
@@ -41,14 +64,14 @@ const TextContent = ({ className = '' }: { className?: string }) => {
 				` ${className}`
 			}
 		>
-			<span className='overline-text text-orange-600 mb-6 md:mb-4'>New product</span>
-			<h2 className='h4 md:h2 mb-6 md:mb-8 max-w-md'>XX99 Mark II Headphones</h2>
-			<p className='body-text mb-6'>
-				The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium
-				headphone experience by reproducing the balanced depth and precision of studio-quality
-				sound.
-			</p>
-			<button className='button-one'>See product</button>
+			{isNew && <span className='overline-text text-orange-600 mb-6 md:mb-4'>New product</span>}
+			<h2 className='h4 md:h2 mb-6 md:mb-8 max-w-md'>{title}</h2>
+			<p className='body-text mb-6'>{description}</p>
+			<Link href={`/product/${slug}`} passHref>
+				<a>
+					<button className='button-one'>See product</button>
+				</a>
+			</Link>
 		</div>
 	)
 }
