c70489f1b0d7070eb79d33dbbaa9fd34405fd7f3
joey desjardins <jwdesjardin@gmail.com>
2021-06-11 13:17:00 -0700

    add responsive styles for section1 component including backgound pattern size and position

--- my-project/components/Home/Section1.tsx
+++ my-project/components/Home/Section1.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 export const Section1 = () => {
 	return (
-		<div className='bg-orange-600 rounded-xl content-container flex flex-col items-center text-center py-[55px] mb-6 background-circles'>
+		<div className='bg-orange-600 rounded-xl content-container flex flex-col lg:flex-row items-center text-center lg:text-left py-[55px] lg:py-[20px] mb-6 background-circles overflow-hidden'>
 			{/* Background svg */}
 
-			<div className='max-w-[180px] mb-8'>
+			<div className='max-w-[180px] md:max-w-[197px] lg:max-w-[390px] lg:ml-[120px] mb-8 md:mb-16 transform translate-y-24'>
 				<picture>
 					<source media='(min-width: 1110px)' srcSet='/assets/home/desktop/image-speaker-zx9.png' />
 					<source media='(min-width: 768px)' srcSet='/assets/home/tablet/image-speaker-zx9.png' />
 					<img src='/assets/home/mobile/image-speaker-zx9.png' alt='' />
 				</picture>
 			</div>
-			<div className='text-white-100 content-container'>
-				<h2 className='h2 mb-6'>ZX9 speaker</h2>
-				<p className='mb-6'>
+			<div className='text-white-100 content-container md:max-w-[350px]'>
+				<h2 className='h2 md:h1 mb-6'>ZX9 speaker</h2>
+				<p className='mb-6 md:mb-10'>
 					Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
 				</p>
-				<button className='button-four'>See product</button>
+				<button className='button-four md:mb-2'>See product</button>
 			</div>
 		</div>
 	)
