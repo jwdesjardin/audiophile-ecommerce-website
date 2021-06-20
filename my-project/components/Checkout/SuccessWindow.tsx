18d36ff9e99e474b2a5a1f8f52e64111f7dbfa8e
joey desjardins <jwdesjardin@gmail.com>
2021-06-20 14:45:56 -0700

    use order context data in checkout success window

--- my-project/components/Checkout/SuccessWindow.tsx
+++ my-project/components/Checkout/SuccessWindow.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { OrderCTX } from '../../context'
 
 export const SuccessWindow = () => {
 	return (
@@ -18,27 +19,56 @@ export const SuccessWindow = () => {
 }
 
 const OrderReview = () => {
+	const { order } = React.useContext(OrderCTX)
+	const [list, setList] = React.useState(order.cartItems.slice(0, 2))
+	const [maxView, toggleMaxView] = React.useState(false)
+
+	React.useEffect(() => {
+		if (maxView) {
+			setList(order.cartItems)
+		} else {
+			setList(order.cartItems.slice(0, 1))
+		}
+	}, [maxView])
+
 	return (
 		<div className='rounded-lg overflow-hidden mb-6 md:mb-12 w-full'>
 			<div className='flex flex-col md:flex-row w-full'>
 				<div className='bg-white-400 p-6 md:w-7/12'>
 					<ul className='w-full border-b border-black-400 mb-2 border-opacity-50'>
-						<li className='flex items-center justify-between mb-2'>
-							<div className='w-[50px] flex-shrink-0'>
-								<img src='/assets/cart/image-xx59-headphones.jpg' alt='' />
-							</div>
-							<div className='w-full ml-4'>
-								<p className='uppercase font-bold'>XX59</p>
-								<p className='text-black-400 font-bold'>$2,999</p>
-							</div>
-							<div className='text-black-400 font-bold'>x1</div>
-						</li>
+						{list.map((item) => (
+							<li className='flex items-center justify-between mb-2'>
+								<div className='w-[50px] flex-shrink-0'>
+									<img src={item.item.cartImage.asset.url} alt='' />
+								</div>
+								<div className='w-full ml-4'>
+									<p className='uppercase font-bold'>{item.item.name}</p>
+									<p className='text-black-400 font-bold'>{item.item.price}</p>
+								</div>
+								<div className='text-black-400 font-bold'>x{item.qty}</div>
+							</li>
+						))}
 					</ul>
-					<button className='text-sm text-black-400 font-bold w-full lowercase'>View Less</button>
+					<button
+						className='text-sm text-black-400 font-bold w-full lowercase'
+						onClick={() => toggleMaxView(!maxView)}
+					>
+						{maxView
+							? 'View Less'
+							: order.cartItems.length > 1
+							? `and ${order.cartItems.length - 1} other item(s)`
+							: ''}
+					</button>
 				</div>
 				<div className='bg-black-900 text-white-200 p-6 md:w-5/12 flex flex-col justify-center'>
 					<p className='uppercase font-extralight mb-2 text-black-400'>Grand Total</p>
-					<p className='h6'>$5,446</p>
+					<p className='h6'>
+						$
+						{order.cartTotals.grandTotal.toLocaleString(undefined, {
+							minimumFractionDigits: 2,
+							maximumFractionDigits: 2,
+						})}
+					</p>
 				</div>
 			</div>
 		</div>
