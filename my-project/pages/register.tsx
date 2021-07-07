9de1cdf762a54c945109cda7bed74790c6fae85e
joey desjardins <jwdesjardin@gmail.com>
2021-07-06 18:35:18 -0700

    setActive user from register page

--- my-project/pages/register.tsx
+++ my-project/pages/register.tsx
@@ -1,15 +1,27 @@
+import axios from 'axios'
 import { Form, Formik } from 'formik'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 import { FormikTextInput } from '../components/Checkout/FormikForm'
 
 import Layout from '../components/Layout'
+import { UserCTX } from '../context'
 
 const register = () => {
+	const router = useRouter()
+	const { setActiveUser } = React.useContext(UserCTX)
+	const [pageError, setPageError] = React.useState<string | null>(null)
 	return (
 		<Layout goBackButton>
 			<div className='content-container flex justify-center'>
-				<div className='mb-[200px] '>
+				<div className='mb-[160px] '>
 					<h2 className='h2 text-center mb-6'>Register</h2>
+					{pageError && (
+						<div className=' absolute top-20 left-1/2 transform -translate-x-1/2 flex-shrink-0 rounded-lg border-red-600 bg-red-600  p-4'>
+							<p className='text-white-200 text-center subtitle mb-4'>{pageError}</p>
+						</div>
+					)}
 					<Formik
 						initialValues={{
 							name: '',
@@ -37,8 +49,24 @@ const register = () => {
 							console.log('values captured')
 
 							setTimeout(() => {
-								alert(JSON.stringify(values, null, 2))
-								setSubmitting(false)
+								;(async () => {
+									const config = { headers: { 'Content-type': 'application/json' } }
+									const res = await axios.post('http://localhost:5000/users/', values, config)
+									if (res.status === 201) {
+										// saves user data to context
+										const user = res.data
+										setActiveUser({
+											_id: user._id,
+											name: user.name,
+											email: user.email,
+											isAdmin: user.isAdmin,
+										})
+										router.push('/')
+									} else if (res.status === 400) {
+										alert('email is already in use. please login.')
+									}
+									setSubmitting(false)
+								})()
 							}, 400)
 						}}
 					>
@@ -77,9 +105,15 @@ const register = () => {
 											/>
 										</div>
 
-										<button className='button-one' disabled={isSubmitting} type='submit'>
+										<button className='button-one mb-6' disabled={isSubmitting} type='submit'>
 											Register
 										</button>
+										<p className='to-black-400 text-center'>
+											dont have an account?{' '}
+											<Link href='/login'>
+												<a className='underline'> please login</a>
+											</Link>
+										</p>
 									</div>
 								</Form>
 							)
